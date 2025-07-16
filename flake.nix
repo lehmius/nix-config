3f3c50245f54414c51f6544edf51dcd2c916d69b
lehmius <git@clajus.eu>
2025-07-16 21:27:23 +0200

    add: home-manager

--- flake.nix
+++ flake.nix
@@ -2,16 +2,29 @@
 	description = "lehmius' nixos system configuration";
 	inputs = {
 		nixpkgs.url = "nixpkgs/nixos-unstable";
+		home-manager = {
+			url = "github:nix-community/home-manager/master";
+			inputs.nixpkgs.follows = "nixpkgs";
+		};
+		
 	};
-	outputs = { self, nixpkgs, ...}:
+	outputs = { self, nixpkgs, home-manager, ...}:
 		let
 			lib = nixpkgs.lib;
+			system = "x86_64-linux";
+			pkgs = nixpkgs.legacyPackages.${system};
 		in {
 			nixosConfigurations = {
 				nixos = lib.nixosSystem {
-					system = "x86_64-linux";
+					inherit system;
 					modules = [ ./configuration.nix ];
 				};
+			};
+			homeConfigurations = {
+				lehmius = home-manager.lib.homeManagerConfiguration {
+					inherit pkgs;
+					modules = [ ./home.nix ];	
+				};	
 			};
 		};
 }
