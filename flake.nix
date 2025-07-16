bb85152109ac68f72036a74686e6f3ced489f505
lehmius <git@clajus.eu>
2025-07-16 14:06:26 +0200

    init

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+	description = "lehmius' nixos system configuration";
+	inputs = {
+		nixpkgs.url = "nixpkgs/nixos-unstable";
+	};
+	outputs = { self, nixpkgs, ...}:
+		let
+			lib = nixpkgs.lib;
+		in {
+			nixosConfigurations = {
+				nixos = lib.nixosSystem {
+					system = "x86_64-linux";
+					modules = [ ./configuration.nix ];
+				};
+			};
+		};
+}
